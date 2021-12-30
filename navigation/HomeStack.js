34bebb5c002388ff1e55b18479a70e8850a2ffc8
Rafee Jenkins <rafeejenkins@gmail.com>
2021-12-30 14:15:53 +0000

    Added profile page and navigation

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import ListView from '../screens/ListView';
 import MapPage from '../screens/MapPage';
+import ProfilePage from '../screens/ProfilePage';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default function HomeStack() {
+function mainStack() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Map" component={MapPage} />
       <Tab.Screen name="List" component={ListView} />
     </Tab.Navigator>
+  );
+}
+
+export default function HomeStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Main" component={mainStack} />
+      <Stack.Screen name="Profile" component={ProfilePage} options={{headerShown: true}}/>
+    </Stack.Navigator>
   );
 }
--- screens/MapPage.js
+++ screens/MapPage.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { Text, StyleSheet, Dimensions } from 'react-native';
+import { Text, StyleSheet, Dimensions, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import MapView from 'react-native-maps';
+import ProfileButton from '../components/ProfileButton';
 
 export default function MapPage() {
+  let tempUser = {
+    name: 'Rafee Jenkins',
+    initials: 'RJ',
+  }
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.SAView}>
       <MapView style={styles.map} />
+      <View pointerEvents='box-none' style={styles.innerView}>
+        <View style={styles.topBar}>
+          <ProfileButton user={tempUser} />
+        </View>
+      </View>
     </SafeAreaView>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  SAView: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  innerView: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%'
+  },
+  topBar: {
+    alignItems: 'flex-end',
+    paddingHorizontal: 10
   },
   map: {
     width: Dimensions.get('window').width,
