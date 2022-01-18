74f4de23f1c107d060c6c54dfe6d4c17c8ce87cb
Rafee Jenkins <rafeejenkins@gmail.com>
2022-01-18 13:26:15 +0000

    Added check for first launch

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import checkIfFirstLaunch from './config/CheckIfFirstLaunched';
 import Routes from './navigation';
 
 export default function App() {
+  
+  const [firstLaunch, setFirstLaunch] = useState(false)
+  
+  async function check() {
+    fl = await checkIfFirstLaunch()
+    setFirstLaunch(fl)
+  }
+  
+  useEffect(() => {
+    check()
+  }, []);
+
   return (
+    firstLaunch ? 
+    <></> :
     <Routes />
   );
 }
