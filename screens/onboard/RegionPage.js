2aebc243367c76d2673d6ee82f49cb11773c4ff8
Rafee Jenkins <rafeejenkins@gmail.com>
2022-01-23 13:44:55 +0000

    Fetch regions from API

--- screens/onboard/RegionPage.js
+++ screens/onboard/RegionPage.js
@@ -1,39 +1,71 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Button, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Picker } from '@react-native-picker/picker';
 
-import { AuthenticatedUserContext } from '../../providers/AuthenticatedUserProvider';
-
 import Firebase from '../../config/firebase';
-import { ResturantContext } from '../../providers/RestaurantProvider';
-import { useNavigation } from '@react-navigation/native';
 
 const auth = Firebase.auth();
+const db = Firebase.firestore();
 
 export default function RegionPage(props) {
     const [region, setRegion] = useState("manchester")
+    const [regions, setRegions] = useState([])
+    const [loading, setLoading] = useState(false)
 
     async function handleSubmit() {
         await AsyncStorage.setItem("Region", region)
         props.callback ? props.callback() : props.navigation.goBack()
     }
 
+    const getRegions = async () => {
+        setLoading(true)
+
+        try {
+            const respHTTP = await fetch("https://halal-dining-uk.web.app/regions")
+            let text = await respHTTP.text()
+
+            const bundlecrap = new global.TextEncoder().encode(text)
+            await db.loadBundle(bundlecrap)
+
+            var data = [];
+            const query = await db.namedQuery("latest-regions-data");
+            await query.get({ source: "cache" })
+                .then((snap) => {
+                    snap.forEach((doc) => {
+                        console.log(doc.data());
+                        data.push(doc.data())
+                    })
+                });
+            setRegions(data)
+            setLoading(false)
+        } catch (error) {
+            console.debug("Failed to get regions")
+            setLoading(false)
+        }
+    }
+
     useEffect(() => {
         // Make a request to google cloud functions to get regions! 
+        getRegions()
     }, [])
 
     return (
         <>
-            <Picker
-                selectedValue={region}
-                onValueChange={(val, idx) => setRegion(val)}
-            >
-                <Picker.Item label={"Manchester"} value={"manchester"} />
-            </Picker>
-            <Button title='Submit' onPress={() => handleSubmit()} />
+            {loading ? <></> :
+                <Picker
+                    selectedValue={region}
+                    onValueChange={(val, idx) => setRegion(val)}
+                >
+                    {
+                        regions.map((val, idx) => {
+                            return <Picker.Item label={val.region} value={val.region} />
+                        })
+                    }
+                </Picker>}
+
+            <Button title='Submit' disabled={loading} onPress={() => handleSubmit()} />
         </>
     );
 }
